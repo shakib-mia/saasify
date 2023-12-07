12d794614f1deedc160bdf3d6a18ebc398957b7b
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-12-07 15:15:36 +0600

    header section is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: {
-      center: true,
-      maxWidth: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1200px",
+    extend: {
+      fontSize: {
+        xxs: "0.875rem",
+        xs: "1rem",
+        sm: "1.125rem",
+        base: "1.1815rem",
+        lg: "1.25rem",
+        xl: "1.3125rem",
+        "2xl": "1.5rem",
+        "3xl": "2.875rem",
+        "4xl": "2.875rem",
+        "5xl": "3.5rem",
+        "6xl": "3.75rem",
+        "7xl": "4.5rem",
+      },
+      colors: {
+        blue: {
+          DEFAULT: "#0F0BC7",
+          dark: "#0D03C3",
+          light: "#2272FF",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          light: "#F6F5FF",
+        },
+        grey: "#E3E2FD",
+        black: "#000000",
+        bg: "#040335",
       },
     },
-
-    extend: {},
   },
   plugins: [],
 };
