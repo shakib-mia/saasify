516d913e62c4e3b9c3cb2fc5d74c8f3089fd67ad
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-12-07 11:23:59 +0600

    container style is added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      maxWidth: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1200px",
+      },
+    },
+
     extend: {},
   },
   plugins: [],
