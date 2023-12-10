228aaeaaa081144e354b1438f7d76fadd3ef57e9
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-12-10 21:22:37 +0600

    video has been added

---
+++ main.js
@@ -0,0 +1,20 @@
+const lenis = new Lenis({
+  duration: 1,
+});
+
+lenis.on("scroll", (e) => {
+  //   console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+document.getElementById("video-check").addEventListener("change", (e) => {
+  if (e.target.checked) {
+    document.getElementById("video-container").style.display = "flex";
+  } else document.getElementById("video-container").style.display = "none";
+});
